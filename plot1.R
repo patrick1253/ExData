90e69ac703f41287c25105139af0397ea154f192
pdata <56500169+patrick1253@users.noreply.github.com>
2019-11-22 18:47:48 +0300

    added comments to explain script

--- plot1.R
+++ plot1.R
@@ -1,15 +1,19 @@
-# plot1.r runs a cript to creat the first plot required for the JHU ExData Assignment 1.
+# plot1.r runs a script to create the first plot required for the JHU ExData Assignment 1.
 
 library(dplyr)
 
+#read data and change class from factor to date/time
 data <- read.csv("household_power_consumption.txt", header=TRUE, sep=";")
 data$Date <- as.Date(data$Date, "%d/%m/%Y")
 data$Time <- as.POSIXct(strptime(data$Time, "%H:%M:%S"))
+
+#subset based on dates of interest
 data2 <- filter(data, data$Date == "2007-02-01" | data$Date == "2007-02-02")
 
-
+#change class from factor to numeric
 data2$Global_active_power <- as.numeric(as.character(data2$Global_active_power))
 
+#create png graphic of histogram and save directly to png file
 png(file = "plot1.png",
     width = 480, height = 480, units = "px")
 hist(data2$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
