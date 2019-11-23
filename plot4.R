cdf3a5c45a7ecc34246583bb2f2ad3101a63ffdb
pdata <56500169+patrick1253@users.noreply.github.com>
2019-11-23 17:13:34 +0300

    Initial commit -- script for Plot 4 of Assignment 1

---
+++ plot4.R
@@ -0,0 +1,50 @@
+# plot4.r runs a script to create the final plot required for the JHU ExData Assignment 1.
+
+library(dplyr)
+
+#read data and change class from factor to date/time
+data <- read.csv("household_power_consumption.txt", header=TRUE, sep=";")
+data$Date <- as.Date(data$Date, "%d/%m/%Y")
+
+#subset based on dates of interest and create new merged datetime variable
+data2 <- filter(data, data$Date == "2007-02-01" | data$Date == "2007-02-02")
+data2$datetime <- strptime(paste(data2$Date, data2$Time), format = "%Y-%m-%d %H:%M:%S")
+
+# open png graphics device and set params
+png(file = "plot4.png",
+    width = 480, height = 480, units = "px")
+par(mfrow = c(2,2))
+
+# PLOT 1,1
+#change class from factor to numeric,create png graphic and save directly to png file
+data2$Global_active_power <- as.numeric(as.character(data2$Global_active_power))
+with(data2, plot(datetime, Global_active_power, type="l", 
+                 ylab = "Global Active Power (kilowatts)", xlab ="")) 
+
+# PLOT 1,2
+#change class from factor to numeric, create png graphic and save directly to png file
+data2$Voltage <- as.numeric(as.character(data2$Voltage))
+with(data2, plot(datetime, Voltage, type="l", 
+                 ylab = "Voltage"))
+
+# PLOT 2,1
+#change class from factor to numeric
+data2$Sub_metering_1 <- as.numeric(as.character(data2$Sub_metering_1))
+data2$Sub_metering_2 <- as.numeric(as.character(data2$Sub_metering_2))
+data2$Sub_metering_3 <- as.numeric(as.character(data2$Sub_metering_3))
+
+#create png graphic and save directly to png file
+
+with(data2, plot(datetime, Sub_metering_1, type="l", 
+                 ylab = "Energy sub metering", xlab =""))
+points(data2$datetime, data2$Sub_metering_2, type = "l", col = "red")
+points(data2$datetime, data2$Sub_metering_3, type = "l", col = "blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       col= c("black", "red", "blue"), lty = c(1,1,1))
+
+#PLOT 2,2
+#change class from factor to numeric, create png graphic and save directly to png file
+data2$Global_reactive_power <- as.numeric(as.character(data2$Global_reactive_power))
+with(data2, plot(datetime, Global_reactive_power, type="l", 
+                 ylab = "Global_reactive_power"))
+dev.off()
